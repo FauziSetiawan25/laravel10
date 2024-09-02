37107451ce261254e57335e2bb8a51be542c6a4f
FauziSetiawan25 <fauzisetiawan2003@mail.ugm.ac.id>
2024-09-02 11:25:20 +0700

    update tugas

---
+++ resources/views/login.blade.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Login</title>
+</head>
+<body>
+    <style>ul{list-style: none;}</style>
+    <h1>Login</h1>
+    <ul>
+        <li>Username :  <input type="text"></li>
+        <li>Email :     <input type="email"></li>
+        <li>Password :  <input type="password"></li>
+    </ul>
+</body>
+</html>
--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\PostController;
+use App\Http\Controllers\LoginController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -37,3 +38,5 @@ Route::get('/contact', function () {
 })->name('contact');
 
 Route::get('/posts', [PostController::class,'index']);
+
+Route::get('/login', [LoginController::class,'index']);
